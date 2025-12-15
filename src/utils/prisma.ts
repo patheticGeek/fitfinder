044451f9582dad88ef0edde63f2e2de1cea0b0bd
Pathetic Geek <geekpathetic@gmail.com>
2025-12-15 23:29:07 +0530

    better hash password

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,5 +1,6 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import crypto from "node:crypto";
+import { promisify } from "node:util";
 import { PrismaClient } from "~/prisma-generated/client";
 
 const adapter = new PrismaPg({
@@ -7,14 +8,20 @@ const adapter = new PrismaPg({
 });
 export const prismaClient = new PrismaClient({ adapter });
 
-export function hashPassword(password: string) {
-  return new Promise<string>((resolve, reject) => {
-    crypto.pbkdf2(password, "salt", 100000, 64, "sha256", (err, derivedKey) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(derivedKey.toString("hex"));
-      }
-    });
-  });
+const cryptoPbkdf2Promise = promisify(crypto.pbkdf2);
+
+export async function hashPassword(password: string) {
+  if (!process.env.PASSWORD_SALT) {
+    throw new Error("PASSWORD_SALT is not defined in environment variables");
+  }
+
+  const derivedKey = await cryptoPbkdf2Promise(
+    password,
+    process.env.PASSWORD_SALT,
+    100000,
+    64,
+    "sha256"
+  );
+
+  return derivedKey.toString("hex");
 }
